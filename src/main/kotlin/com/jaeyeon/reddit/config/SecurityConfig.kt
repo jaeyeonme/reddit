52dd53f17828d276e218261843588fd1865325d6
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-12-26 17:40:14 +0900

    [feature] Member DTO 구현

---
+++ src/main/kotlin/com/jaeyeon/reddit/config/SecurityConfig.kt
@@ -0,0 +1,11 @@
+package com.jaeyeon.reddit.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+}
